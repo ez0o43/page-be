8626ceb40f3b46a40ee180645b1b2320c0047a59
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-07-09 20:34:33 +0900

    Refactor: NoticeBoardService

--- src/main/java/KKSC/page/domain/notice/service/NoticeBoardService.java
+++ src/main/java/KKSC/page/domain/notice/service/NoticeBoardService.java
@@ -1,10 +1,8 @@
 package KKSC.page.domain.notice.service;
 
-import KKSC.page.domain.member.entity.Member;
 import KKSC.page.domain.notice.dto.NoticeBoardDetailResponse;
 import KKSC.page.domain.notice.dto.NoticeBoardListResponse;
 import KKSC.page.domain.notice.dto.NoticeBoardRequest;
-import KKSC.page.domain.notice.entity.NoticeBoard;
 
 import java.util.List;
 
@@ -18,7 +16,7 @@ public interface NoticeBoardService {
 
     List<NoticeBoardListResponse> getBoardList(); /* 기본적으로 보여줄 게시글 목록 */
 
-    NoticeBoard getBoardDetail(Long noticeBoardId); /* 글 선택 시 보여줄 상세 글 */
+    NoticeBoardDetailResponse getBoardDetail(Long noticeBoardId); /* 글 선택 시 보여줄 상세 글 */
 
     List<NoticeBoardListResponse> searchBoardList(String cmd); /* 작성일 순, 작성자가 포함된 글 불러오기 */
 }

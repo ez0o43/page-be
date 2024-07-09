85076ebd302313635583028b2e87349fe0450e81
Daeun <dddddaeun88.88@gmail.com>
2024-07-09 22:00:41 +0900

    Refactor: NoticeBoardResponseDto

--- src/main/java/KKSC/page/domain/notice/dto/NoticeBoardDetailResponse.java
+++ src/main/java/KKSC/page/domain/notice/dto/NoticeBoardDetailResponse.java
@@ -1,14 +1,29 @@
 package KKSC.page.domain.notice.dto;
 
+import KKSC.page.domain.notice.entity.NoticeBoard;
+
 import java.time.LocalDateTime;
 
 public record NoticeBoardDetailResponse(
         String title,
         String content,
-        String createdBy,
+//        String createdBy,
         Long view,
         Long delYN,
-        Long fileId,
-        String fileName,
+//        Long fileId,
+//        String fileName,
         LocalDateTime createdAt,
-        LocalDateTime modifiedAt) {}
+        LocalDateTime modifiedAt
+) {
+    // entity -> dto
+    public NoticeBoardDetailResponse (NoticeBoard noticeBoard){
+        this (
+                noticeBoard.getTitle(),
+                noticeBoard.getContent(),
+                noticeBoard.getView(),
+                noticeBoard.getDelYN(),
+                noticeBoard.getCreatedAt(),
+                noticeBoard.getModifiedAt()
+        );
+    }
+}
--- src/main/java/KKSC/page/domain/notice/dto/NoticeBoardListResponse.java
+++ src/main/java/KKSC/page/domain/notice/dto/NoticeBoardListResponse.java
@@ -1,12 +1,24 @@
 package KKSC.page.domain.notice.dto;
 
+import KKSC.page.domain.notice.entity.NoticeBoard;
+
 import java.time.LocalDateTime;
 
 public record NoticeBoardListResponse (
         String title,
-        String name,
-        Long fileYN,
+//        String name,
+//        Long fileYN,
         Long view,
         Long delYN,
         LocalDateTime createdAt
-) {}
+) {
+    // entity ->  dto
+    public NoticeBoardListResponse(NoticeBoard noticeBoard) {
+        this(
+                noticeBoard.getTitle(),
+                noticeBoard.getView(),
+                noticeBoard.getDelYN(),
+                noticeBoard.getCreatedAt()
+        );
+    }
+}

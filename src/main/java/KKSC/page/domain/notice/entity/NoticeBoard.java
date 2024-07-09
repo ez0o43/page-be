23a35968af8fc507fac548bc7f826584b2ffafcd
Daeun <dddddaeun88.88@gmail.com>
2024-07-09 22:00:57 +0900

    Refactor: NoticeBoard

--- src/main/java/KKSC/page/domain/notice/entity/NoticeBoard.java
+++ src/main/java/KKSC/page/domain/notice/entity/NoticeBoard.java
@@ -1,12 +1,9 @@
 package KKSC.page.domain.notice.entity;
 
-import KKSC.page.domain.member.entity.Member;
+import KKSC.page.domain.notice.dto.NoticeBoardRequest;
 import KKSC.page.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
@@ -28,5 +25,13 @@ public class NoticeBoard extends BaseTimeEntity {
 
     private Long view; /* 조회수 */
     private Long fixed; /* 상단 고정 여부 */
+
+    @Setter
     private Long delYN; /* 삭제 여부 */
+
+    public void update(NoticeBoardRequest noticeBoardRequest) {
+        this.title = noticeBoardRequest.title();
+        this.content = noticeBoardRequest.content();
+        this.fixed = noticeBoardRequest.fixed();
+    }
 }

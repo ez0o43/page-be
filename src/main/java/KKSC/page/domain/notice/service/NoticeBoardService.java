a6ae45404ad663d485dd2b918a31290fa9230dbe
Daeun <dddddaeun88.88@gmail.com>
2024-07-09 21:59:22 +0900

    Refactor: NoticeBoardService

--- src/main/java/KKSC/page/domain/notice/service/NoticeBoardService.java
+++ src/main/java/KKSC/page/domain/notice/service/NoticeBoardService.java
@@ -1,6 +1,5 @@
 package KKSC.page.domain.notice.service;
 
-import KKSC.page.domain.member.entity.Member;
 import KKSC.page.domain.notice.dto.NoticeBoardDetailResponse;
 import KKSC.page.domain.notice.dto.NoticeBoardListResponse;
 import KKSC.page.domain.notice.dto.NoticeBoardRequest;
@@ -10,15 +9,15 @@ import java.util.List;
 
 public interface NoticeBoardService {
 
-    void create(Member member, NoticeBoardRequest noticeBoardRequest);
+    NoticeBoard create(NoticeBoardRequest noticeBoardRequest);
 
-    void update(Member member, Long noticeBoardId, NoticeBoardRequest noticeBoardRequest);
+    void update(Long noticeBoardId, NoticeBoardRequest noticeBoardRequest);
 
-    void delete(Member member, Long noticeBoardId);
+    void delete(Long noticeBoardId);
 
     List<NoticeBoardListResponse> getBoardList(); /* 기본적으로 보여줄 게시글 목록 */
 
-    NoticeBoard getBoardDetail(Long noticeBoardId); /* 글 선택 시 보여줄 상세 글 */
+    NoticeBoardDetailResponse getBoardDetail(Long noticeBoardId); /* 글 선택 시 보여줄 상세 글 */
 
     List<NoticeBoardListResponse> searchBoardList(String cmd); /* 작성일 순, 작성자가 포함된 글 불러오기 */
 }
--- src/main/java/KKSC/page/domain/notice/service/impl/NoticeBoardServiceImpl.java
+++ src/main/java/KKSC/page/domain/notice/service/impl/NoticeBoardServiceImpl.java
@@ -1,6 +1,6 @@
 package KKSC.page.domain.notice.service.impl;
 
-import KKSC.page.domain.member.entity.Member;
+import KKSC.page.domain.notice.dto.NoticeBoardDetailResponse;
 import KKSC.page.domain.notice.dto.NoticeBoardListResponse;
 import KKSC.page.domain.notice.dto.NoticeBoardRequest;
 import KKSC.page.domain.notice.entity.NoticeBoard;
@@ -9,6 +9,7 @@ import KKSC.page.domain.notice.service.NoticeBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,36 +19,43 @@ public class NoticeBoardServiceImpl implements NoticeBoardService {
     private final NoticeBoardRepository noticeBoardRepository;
 
     @Override
-    public void create(Member member, NoticeBoardRequest noticeBoardRequest) {
+    public NoticeBoard create(NoticeBoardRequest noticeBoardRequest) {
         /*
          * 사용자에게 공지사항 게시판을 작성할 권한이 있는지 확인
-         * noticeBoardReqeust를 통해 noticeBoard 객체 생성
-         * noticeBoardrepository에 저장
+         * noticeBoardRequest 를 통해 noticeBoard 객체 생성
+         * noticeBoardRepository 에 저장
          * 성공적으로 저장되었다면 200 OK
          */
+        return noticeBoardRepository.save(noticeBoardRequest.toEntity());
     }
 
     @Override
-    public void update(Member member, Long noticeBoardId, NoticeBoardRequest noticeBoardRequest) {
+    public void update(Long noticeBoardId, NoticeBoardRequest noticeBoardRequest) {
         /*
          * 사용자에게 수정할 권한이 있는지 확인
          * noticeBoardRepository에서 noticeBoardId로 기존 수정할 게시글 가져오기
-         * noticeBoardRequest의 내용으로 수정
+         * noticeBoardRequest 내용으로 수정
          * 성공적으로 수정되었다면 NoticeBoardResponse에 수정한 내용 담아서 반환
          */
 
         /* 1. 권한 확인
            2. title, content, updateAt 업데이트
            3. 변경사항 저장 save() */
+        NoticeBoard noticeBoard = noticeBoardRepository.findById(noticeBoardId).orElseThrow();
+        noticeBoard.update(noticeBoardRequest);
+        noticeBoardRepository.save(noticeBoard);
     }
 
     @Override
-    public void delete(Member member, Long noticeBoardId) {
+    public void delete(Long noticeBoardId) {
         /*
          * 사용자에게 삭제할 권한이 있는지 확인
-         * 삭제할 noticeBoardId를 repository에서 찾아옴
+         * 삭제할 noticeBoardId를 repository 에서 찾아옴
          * del_YN의 여부를 바꾼 뒤 200 OK 반환
          */
+        NoticeBoard noticeBoard = noticeBoardRepository.findById(noticeBoardId).orElseThrow();
+        noticeBoard.setDelYN(noticeBoard.getId());
+        noticeBoardRepository.delete(noticeBoard);
     }
 
     @Override
@@ -55,17 +63,25 @@ public class NoticeBoardServiceImpl implements NoticeBoardService {
         /*
          * repository Native Query 작성
          */
-        return List.of();
+//        return List.of();
+        List<NoticeBoard> noticeBoards = noticeBoardRepository.findAll();
+        List<NoticeBoardListResponse> listResponses = new ArrayList<>();
+        for (NoticeBoard noticeBoard : noticeBoards) {
+            listResponses.add(new NoticeBoardListResponse(noticeBoard));
+        }
+        return listResponses;
     }
 
     @Override
-    public NoticeBoard getBoardDetail(Long noticeBoardId) {
+    public NoticeBoardDetailResponse getBoardDetail(Long noticeBoardId) {
         /*
          * repository Native Query 작성
          * noticeBoardId의 글을 repo에서 가져와서 response에 담아서 반환
          * 성공적으로 조회되었다면 200 OK
          */
-        return null;
+        NoticeBoard noticeBoard = noticeBoardRepository.findById(noticeBoardId).orElseThrow(null);
+        NoticeBoardDetailResponse detailResponse = new NoticeBoardDetailResponse(noticeBoard);
+        return detailResponse;
     }
 
     @Override

440a751d69ce85977f9c9e04f8f5b19fb84c479c
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-07-12 16:46:36 +0900

    Refactor: Configuration

--- src/main/java/KKSC/page/domain/member/entity/Member.java
+++ src/main/java/KKSC/page/domain/member/entity/Member.java
@@ -1,13 +1,10 @@
 package KKSC.page.domain.member.entity;
 
-import KKSC.page.domain.notice.entity.NoticeBoard;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Entity
 @Getter
@@ -20,8 +17,5 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @OneToMany(mappedBy = "member")
-    private List<NoticeBoard> noticeBoards;
-
-    private String name;
+    private String username;
 }

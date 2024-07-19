4a2b2e0df289c618373c81dd6882741e4115e571
krx00703_j.h.lee <krx00703@navercorp.com>
2024-07-19 17:16:42 +0900

    feat : FileUtil 기능 생성

--- src/main/java/KKSC/page/domain/notice/service/NoticeFileService.java
+++ src/main/java/KKSC/page/domain/notice/service/NoticeFileService.java
@@ -1,7 +1,6 @@
 package KKSC.page.domain.notice.service;
 
 import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public interface NoticeFileService {

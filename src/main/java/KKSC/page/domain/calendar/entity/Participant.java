7d09e13facd6332ed4e259346527a5e0a8f941ca
ChoiColin <cgh16581658@gmail.com>
2024-07-26 13:30:46 +0900

    FEAT: Calendar Entity 작성

---
+++ src/main/java/KKSC/page/domain/calendar/entity/Participant.java
@@ -0,0 +1,31 @@
+package KKSC.page.domain.calendar.entity;
+
+import KKSC.page.domain.member.entity.Member;
+import KKSC.page.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Participant extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "participant_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "calendar_id")
+    private Calendar calendar;
+
+    @OneToOne(mappedBy = "member")
+    private List<Member> members;
+
+}

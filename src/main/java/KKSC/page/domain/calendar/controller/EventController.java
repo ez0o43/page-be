ecbad4d2f3a7aa69b76ba9b2dff64b8e99a5eeab
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-07-30 20:14:05 +0900

    Refactor: EventController

--- src/main/java/KKSC/page/domain/calendar/controller/EventController.java
+++ src/main/java/KKSC/page/domain/calendar/controller/EventController.java
@@ -14,37 +14,37 @@ public class EventController {
 
     // 캘린더 일정 추가
     @PostMapping("/")
-    public void addEvent() {
-        eventService.createSchedule();
+    public void addEvent(@RequestBody EventRequest eventRequest) {
+        eventService.createSchedule(eventRequest);
     }
 
     // 캘린더 일정 삭제
     @DeleteMapping("/{id}")
     public void deleteEvent(@RequestParam Long id) {
-        eventService.deleteSchedule();
+        eventService.deleteSchedule(id);
     }
 
     // 캘린더 일정 수정
     @PutMapping("/{id}")
     public void updateEvent(@RequestParam Long id, @RequestBody EventRequest eventRequest) {
-        eventService.updateSchedule();
+        eventService.updateSchedule(id, eventRequest);
     }
 
     // 캘린더 일정 참가
     @PostMapping("/{id}/join")
     public void joinEvent(@RequestParam Long id) {
-        eventService.joinSchedule();
+        eventService.joinSchedule(id, null); // name 수정 필요
     }
 
     // 캘린더 일정 참가 취소
     @DeleteMapping("/cancel/{id}")
     public void cancelEvent(@RequestParam Long id) {
-        eventService.cancelSchedule();
+        eventService.cancelSchedule(id);
     }
 
     // 캘린더 일정 목록 조회
     @GetMapping("/")
-    public void getEventList() {
-        eventService.getScheduleList();
+    public void getEventList(@RequestParam Long year, @RequestParam Long month) {
+        eventService.getScheduleList(year, month);
     }
 }

d0779bf093e57e4c4d46744275f16c5e1920e754
passionryu <rsy1225@naver.com>
2024-08-01 14:38:25 +0900

    feat: 3조 Method 권한처리 요구사항 작업 완료

--- src/main/java/KKSC/page/domain/calendar/repoAndService/EventService.java
+++ src/main/java/KKSC/page/domain/calendar/repoAndService/EventService.java
@@ -1,27 +1,34 @@
 package KKSC.page.domain.calendar.repoAndService;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public interface EventService {
     
-    // 일정 목록 조회    
+    // 일정 목록 조회
+    @PreAuthorize("hasRole('permission_level1 ')")
     Long getScheduleList();
 
     // 일정 생성
+    @PreAuthorize("hasRole('permission_level0 ')")
     Long createSchedule();
 
     // 일정 삭제
+    @PreAuthorize("hasRole('permission_level0 ')")
     Long deleteSchedule();
 
     // 일정 수정
+    @PreAuthorize("hasRole('permission_level0 ')")
     Long updateSchedule();
 
     // 일정 참가
+    @PreAuthorize("hasRole('permission_level1 ')")
     Long joinSchedule();
 
     // 일정 참가 취소
+    @PreAuthorize("hasRole('permission_level1 ')")
     Long cancelSchedule ();
 
 }

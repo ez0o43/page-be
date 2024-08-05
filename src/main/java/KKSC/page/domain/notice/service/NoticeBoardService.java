9ea6aa5420bc68673702e54fb9a3f32dff78c60d
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-08-05 14:56:25 +0900

    Test: 테스트 코드 권한 추가

--- src/main/java/KKSC/page/domain/notice/service/NoticeBoardService.java
+++ src/main/java/KKSC/page/domain/notice/service/NoticeBoardService.java
@@ -10,22 +10,23 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface NoticeBoardService {
-    @PreAuthorize("hasRole('permission_level0 ')")
+
+    @PreAuthorize("hasRole('permission_level0')")
     Long create(NoticeBoardRequest noticeBoardRequest, String memberName);
 
-    @PreAuthorize("hasRole('permission_level0 ')")
+    @PreAuthorize("hasRole('permission_level0')")
     NoticeBoardDetailResponse update(Long noticeBoardId, NoticeBoardRequest noticeBoardRequest);
 
-    @PreAuthorize("hasRole('permission_level0 ')")
+    @PreAuthorize("hasRole('permission_level0')")
     void delete(Long noticeBoardId);
 
-    @PreAuthorize("hasRole('permission_level1 ')")
+    @PreAuthorize("hasRole('permission_level1')")
     Page<NoticeBoardListResponse> getBoardList(Pageable pageable); /* 기본적으로 보여줄 게시글 목록 */
 
-    @PreAuthorize("hasRole('permission_level1 ')")
+    @PreAuthorize("hasRole('permission_level1')")
     NoticeBoardDetailResponse getBoardDetail(Long noticeBoardId); /* 글 선택 시 보여줄 상세 글 */
 
-    @PreAuthorize("hasRole('permission_level1 ')")
+    @PreAuthorize("hasRole('permission_level1')")
     Page<NoticeBoardListResponse> searchBoardList(Keyword keyword, String query, Pageable pageable); /* 작성일 순, 작성자가 포함된 글 불러오기 */
 
     void countUpView(Long noticeBoardId);

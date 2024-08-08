be11e9ab155cbaed463d89360370b43cd72eb25a
ez0o43 <zxpsej43@gmail.com>
2024-08-08 22:08:55 +0900

    Update MemberRequest.java

--- src/main/java/KKSC/page/domain/member/dto/request/MemberRequest.java
+++ src/main/java/KKSC/page/domain/member/dto/request/MemberRequest.java
@@ -22,13 +22,13 @@ public record MemberRequest(
 
         @NotNull
         @Size(max = 20, message = "학번은 최대 20자까지 가능합니다.")
-        String studentId,
+        String studentId
 
-        @Size(max = 200, message = "자기소개는 최대 200자까지 가능합니다.")
+       /* @Size(max = 200, message = "자기소개는 최대 200자까지 가능합니다.")
         String intro,
 
         @Size(min = 2, max = 10, message = "별명은 최대 10자까지 가능합니다.")
-        String nickname
+        String nickname*/
 ) {
     public Member toEntity() {
         return Member.builder()

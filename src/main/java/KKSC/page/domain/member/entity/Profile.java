288b7bbde45ab2660d2005b070db6d404fca4036
ez0o43 <zxpsej43@gmail.com>
2024-08-08 22:15:53 +0900

    Update Profile.java

--- src/main/java/KKSC/page/domain/member/entity/Profile.java
+++ src/main/java/KKSC/page/domain/member/entity/Profile.java
@@ -1,6 +1,6 @@
 package KKSC.page.domain.member.entity;
 
-import KKSC.page.domain.member.dto.request.MemberRequest;
+import KKSC.page.domain.member.dto.request.ProfileRequest;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,28 +9,46 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@ToString(of = {"id", "intro", "nickname", "profilePhotoPath"})
+
 public class Profile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
     private Long id;
 
     @OneToOne(mappedBy = "profile")
     private Member member;
 
-    /**
-     * photo 추가 예정
-     */
-
-    //자기소개
+    @Column(name = "intro", length = 200)
     private String intro;
 
     @Column(name = "nickname", length = 50)
     private String nickname;
 
-    public void update(MemberRequest memberRequest) {
-        this.intro = memberRequest.intro();
-        this.nickname = memberRequest.nickname();
-        //포토 추가예정
+    @Column(name = "profile_photo_path", length = 255 )
+    private String profilePhotoPath;
+
+    public void setIntro(String introduction) {
+        this.intro = introduction;
     }
+
+    public void setProfilePhotoPath(String profilePhotoPath) {
+        this.profilePhotoPath = profilePhotoPath;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+        if (member != null && member.getProfile() != this) {
+            member.setProfile(this);
+        }
+    }
+
+    public void updateProfile(ProfileRequest profileRequest) {
+        this.intro = profileRequest.intro();
+        this.nickname = profileRequest.nickname();
+        this.profilePhotoPath = profileRequest.profilePhotoPath();
+    }
+
 }
